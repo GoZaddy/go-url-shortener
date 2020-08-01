06dd86a1eda4984bdc17f6a150cad26b0fceadae
Yusuff Faruq <faruqyusuff437@gmail.com>
2020-08-01 02:57:03 +0100

    finished UI

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"math/rand"
 	"net/http"
+	"net/url"
+
 	"time"
 
 	"github.com/gozaddy/go-url-shortener/models"
@@ -15,6 +17,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	currentLink string = ""
 )
 
 func init() {
@@ -46,7 +52,12 @@ func main() {
 	router.LoadHTMLGlob("views/*")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.gohtml", nil)
+		if currentLink == "" {
+			c.HTML(200, "index.gohtml", nil)
+		} else {
+			c.HTML(200, "index.gohtml", currentLink)
+		}
+
 	})
 
 	router.GET("/:linkID", func(c *gin.Context) {
@@ -70,18 +81,41 @@ func main() {
 	})
 
 	router.POST("/api/shorten", func(c *gin.Context) {
-		var url struct {
+		var urlInRequest struct {
 			Link string `form:"link" json:"link" binding:"required"`
 		}
-		if err := c.ShouldBindJSON(&url); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "bad request"})
+
+		if c.Request.Header.Get("Content-Type") == "application/x-www-form-urlencoded" {
+			urlInRequest.Link = c.PostForm("link")
+		} else {
+			if err := c.ShouldBindJSON(&urlInRequest); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "bad request"})
+				return
+			}
+		}
+		if _, err := url.ParseRequestURI(urlInRequest.Link); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid url"})
 			return
 		}
 		randomID := generateRandomString()
 
 		urlModel := models.URL{
 			ID:          randomID,
-			OriginalURL: url.Link,
+			OriginalURL: urlInRequest.Link,
+		}
+
+		for {
+			res := linksCol.FindOne(context.Background(), bson.M{"link_id": randomID})
+			if res.Err() != nil {
+				if errors.Is(res.Err(), mongo.ErrNoDocuments) {
+					break
+				}
+				c.JSON(500, gin.H{
+					"error": "Internal Server error!",
+				})
+				return
+			}
+			randomID = generateRandomString()
 		}
 
 		_, err := linksCol.InsertOne(context.Background(), urlModel)
@@ -92,9 +126,13 @@ func main() {
 			return
 		}
 
-		c.JSON(200, gin.H{
+		/*c.JSON(200, gin.H{
 			"url": "http://localhost:4000/" + randomID,
-		})
+		})*/
+
+		currentLink = "http://localhost:4000/" + randomID
+
+		c.Redirect(http.StatusSeeOther, "/")
 
 	})
 
--- models/url.go
+++ models/url.go
@@ -2,6 +2,6 @@ package models
 
 //URL model
 type URL struct {
-	ID          string `bson:"link_id"`
-	OriginalURL string `bson:"original_url"`
+	ID          string `bson:"link_id" validate:"required"`
+	OriginalURL string `bson:"original_url" validate:"url, required"`
 }
